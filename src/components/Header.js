4235589774027b368eb84bd13a257e4e9631799e
Apurv Jain <jainapurv35@gmail.com>
2023-04-09 18:25:59 +0530

    responsive blogs

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,15 +13,15 @@ const Header = () => {
   const [menu,setMenu] = useState(false);
   return (
     <div>
-        <div className='flex max-[767px]:block min-[767px]:max-[1024px]:pt-[30px]'>
-            <div className='pl-[35px] pr-[20px] w-[18%] max-[767px]:w-[100%] max-[767px]:pt-[30px] h-[]'>
+        <div className='flex max-sm:block sm:max-md:pt-[30px]'>
+            <div className='pl-[35px] pr-[20px] w-[18%] max-sm:w-[100%] max-sm:pt-[30px] h-[]'>
                 <a href=''>
-                    <img src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/Next_Solutions-removebg-preview-1.png' className='w-[160px] max-[767px]:w-[70%] max-[767px]:mx-auto max-[1024px]:h-[auto] min-[767px]:max-[1024px]:pt-[30px] h-[124px]'></img>
+                    <img src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/Next_Solutions-removebg-preview-1.png' className='w-[160px] max-sm:w-[70%] max-sm:mx-auto max-md:h-[auto] sm:max-md:pt-[30px] h-[124px]'></img>
                 </a>
             </div>
             <div className='w-[100%]'>
-                <div className='flex max-[767px]:block max-[767px]:mx-auto'>
-                    <div className='bg-[#F4F5F8] px-[10px] py-[5px] flex gap-[25px] w-[80%] max-[767px]:w-[100%] max-[767px]:justify-center'>
+                <div className='flex max-sm:block max-sm:mx-auto'>
+                    <div className='bg-[#F4F5F8] px-[10px] py-[5px] flex gap-[25px] w-[80%] max-sm:w-[100%] max-sm:justify-center'>
                         <div className='flex items-center gap-[15px]'>
                             <MailOutlineIcon sx={{color:'#D9157F',fontSize:'18px'}} ></MailOutlineIcon>
                             <p className='text-[#9B9FA7] text-[13px]'>info@nextsolutions.com</p>
@@ -31,28 +31,28 @@ const Header = () => {
                             <p className='text-[#9B9FA7] text-[13px]'>Faridabad, Haryana</p>
                         </div>
                     </div>
-                    <div className='flex justify-center items-center w-[20%] max-[767px]:w-[100%] gap-[20px] py-[15px]'>
+                    <div className='flex justify-center items-center w-[20%] max-sm:w-[100%] gap-[20px] py-[15px]'>
                         <a href=''><FacebookRoundedIcon fontSize='small'></FacebookRoundedIcon></a>
                         <a href=''><TwitterIcon fontSize='small'></TwitterIcon></a>
                         <a href=''><YouTubeIcon fontSize='small'></YouTubeIcon></a>
                     </div>
                 </div>
-                <div className='flex justify-between px-[10px] max-[1024px]:mb-[40px] min-[767px]:max-[1024px]:mt-[10px] items-center font-serif'>
-                    <div className='max-[1024px]:hidden'><Navbar/></div>
-                    <div className='flex max-[1024px]:justify-end max-[1024px]:w-[100%] items-center'>
-                        <div className='mr-[10px] min-[1024px]:hidden'>
+                <div className='flex justify-between px-[10px] max-md:mb-[40px] sm:max-md:mt-[10px] items-center font-serif'>
+                    <div className='max-md:hidden'><Navbar/></div>
+                    <div className='flex max-md:justify-end max-md:w-[100%] items-center'>
+                        <div className='mr-[10px] md:hidden'>
                             {!menu&&<IconButton  onClick={()=>setMenu(!menu)}><MenuIcon sx={{color:"#494c4f",width:"30px",height:"30px"}}></MenuIcon></IconButton>}
                             {menu&&<IconButton onClick={()=>setMenu(!menu)}><CloseIcon sx={{color:"#494c4f",width:"30px",height:"30px"}}></CloseIcon></IconButton>}
                         </div>
-                        <div className='max-[767px]:w-[65%] max-[1024px]:w-[18%] mr-[20px]'>
-                            <button className='dmsans py-[7px] min-[767px]:px-[30px] min-[1024px]:px-[70px] w-[100%] bg-[#D9157F] font-medium text-[#fff] text-[15px] rounded-[5px]'>Get Solution</button>
+                        <div className='max-sm:w-[65%] max-md:w-[18%] mr-[20px]'>
+                            <button className='dmsans py-[7px] sm:px-[30px] md:px-[70px] w-[100%] bg-[#D9157F] font-medium text-[#fff] text-[15px] rounded-[5px]'>Get Solution</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         {menu&&
-                <div className='z-20 duration-350 ease-in min-[1024px]:hidden bg-[#000] w-[100%] max-[767px]:border-t border-black text-[16px] font-medium dmsans text-left text-[#494c4f]'>
+                <div className='z-20 duration-350 ease-in md:hidden bg-[#000] w-[100%] max-sm:border-t border-black text-[16px] font-medium dmsans text-left text-[#494c4f]'>
                     <div className={`${window.location.pathname==="/"?'bg-[#e8c204]':'bg-[#f9f9f9]'} hover:bg-[#e8c204] hover:text-[#fff] duration-350 ease-out hover:ease-in`}><a href='/' className='block py-[9px] pl-[20px]'>Home</a></div>
                     <div className={`${window.location.pathname==="/about"?'bg-[#e8c204]':'bg-[#f9f9f9]'} hover:bg-[#e8c204] hover:text-[#fff] duration-300 ease-out hover:ease-in`}><a href='/about' className='block py-[9px] pl-[20px]'>About Us</a></div>
                     <div className={`${window.location.pathname==="/services"?'bg-[#e8c204]':'bg-[#f9f9f9]'} hover:bg-[#e8c204] hover:text-[#fff] duration-300 ease-out hover:ease-in`}><a href='/services' className='block py-[9px] pl-[20px]'>Services</a></div>
--- src/pages/Blog2.js
+++ src/pages/Blog2.js
@@ -1,43 +1,43 @@
 import React from 'react'
 
-const Blog2 = (props) => {
+const Blog1 = (props) => {
   return (
     <div className='spacesek'>
         <div className='bg-[url("https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/page-title.jpg?time=1680010227")] bg-no-repeat bg-center bg-cover flex justify-center items-center h-[422px]'>
             <p className='text-[#fff] text-[60px] font-semibold'>Necessity may give us best virtual court</p>
         </div>
-        <div className='mt-[90px] ml-[200px] mb-[20px]'>
+        <div className='mt-[90px] md:ml-[200px] max-md:mx-[10px] mb-[20px]'>
             <img src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/news2-672x448-1.jpg' alt=''></img>
         </div>
-        <div className='pl-[200px] text-left'>
-            <p className='text-[#1e293b] font-semibold text-left text-[25px]'>We’re Leader In Organic Market</p>
-            <p className='text-[18px] leading-[30px]'>The passage experienced a surge in during the 1960s when Letraset used it on their dry-transfer sheets,<br></br>
-            and again during the 90s as desktop publishers bundled the text with their software. Today it’s seen all <br></br>
+        <div className='md:pl-[200px] max-md:mx-[10px] text-left max-w-[1140px]'>
+            <p className='text-[#1e293b] font-semibold text-left text-[25px] max-md:text-[20px] plusjak'>We’re Leader In Organic Market</p>
+            <p className='text-[18px] leading-[30px] dmsans max-md:text-[17px]'>The passage experienced a surge in during the 1960s when Letraset used it on their dry-transfer sheets,
+            and again during the 90s as desktop publishers bundled the text with their software. Today it’s seen all 
             around the web; on templates, websites, and stock designs</p>
-            <p className='text-[#1e293b] mt-[30px] font-semibold text-[25px]'>Our Personal Approach</p>
-            <div className='flex gap-[25px]'>
-                <img className='w-[282px] h-[188px]' src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/bg12-1536x1024-1.jpg' alt=''></img>
-                <img className='w-[282px] h-[188px]' src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/bg16-1536x1024-1.jpg' alt=''></img>
-                <img className='w-[282px] h-[188px]' src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/service5-1536x1024-1.jpg' alt=''></img>
+            <p className='text-[#1e293b] mt-[30px] font-semibold text-[25px] max-md:text-[20px] plusjak'>Our Personal Approach</p>
+            <div className='sm:flex gap-[25px]'>
+                <img className='sm:w-[27%] sm:h-[188px] max-[sm]:w-[100%] max-sm:h-[auto] max-sm:mb-[20px]' src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/bg12-1536x1024-1.jpg' alt=''></img>
+                <img className='sm:w-[27%] sm:h-[188px] max-[sm]:w-[100%] max-sm:h-[auto] max-sm:mb-[20px]' src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/bg16-1536x1024-1.jpg' alt=''></img>
+                <img className='sm:w-[27%] sm:h-[188px] max-[sm]:w-[100%] max-sm:h-[auto]' src='https://4ni06c.n3cdn1.secureserver.net/wp-content/uploads/2023/03/service5-1536x1024-1.jpg' alt=''></img>
             </div>
-            <p className='text-[#1e293b] mt-[25px] font-semibold text-left text-[25px]'>Latest Articles Updated Daily</p>
-            <p className='text-[18px] leading-[30px] mb-[10px]'>Pellentesque dignissim malesuada varius et semper semper rutrum ad risus felis 
-            eros. Cursus libero viverra <br></br>tempus netus diam vestibulum lorem tincidunt congue porta. Non ligula egestas commodo massa.
-             Lorem<br></br> non gravida risus felis rutrum Non ligula sempe</p>
+            <p className='text-[#1e293b] mt-[25px] font-semibold text-left text-[25px] max-md:text-[20px] plusjak'>Latest Articles Updated Daily</p>
+            <p className='text-[18px] leading-[30px] mb-[10px] dmsans max-md:text-[17px]'>Pellentesque dignissim malesuada varius et semper semper rutrum ad risus felis 
+            eros. Cursus libero viverra tempus netus diam vestibulum lorem tincidunt congue porta. Non ligula egestas commodo massa.
+             Lorem non gravida risus felis rutrum Non ligula sempe</p>
             <div className='m-[30px] p-[20px] text-[#67768e] border-l-[5px] border-solid'>
-                <blockquote>Diam luctus nostra dapibus varius et semper semper rutrum ad risus felis eros. Cursus <br></br>libero viverra 
-                tempus netus diam vestibulum lorem tincidunt congue porta. Non ligula <br></br>egestas commodo massa. Lorem non vivamus Non 
+                <blockquote className='dmsans max-md:text-[18px] dmsans'>Diam luctus nostra dapibus varius et semper semper rutrum ad risus felis eros. Cursus libero viverra 
+                tempus netus diam vestibulum lorem tincidunt congue porta. Non ligula egestas commodo massa. Lorem non vivamus Non 
                 Cursus tempus- Harris Rackham</blockquote>
             </div>
         </div>
-        <div className='pl-[200px] mt-[30px]'>
-            <iframe src='https://www.youtube.com/embed/hddwAIXbKZo?feature=oembed' title='2K Video Demo' loading='lazy' width="900px" height="500px"></iframe>
+        <div className='md:pl-[200px] max-md:mx-[10px] mt-[30px]'>
+            <iframe className='md:w-[900px] md:h-[500px] max-sm:w-[100%] max-sm:h-[358px] sm:max-md:w-[79%] sm:max-md:h-[380px]' src='https://www.youtube.com/embed/hddwAIXbKZo?feature=oembed' title='2K Video Demo' loading='lazy'></iframe>
         </div>
-        <div className='pl-[200px] mt-[10px] mb-[100px] text-left'>
-            <p className='text-[#1e293b] font-semibold text-left text-[25px]'>Making Perfect Placeholder</p>
-            <ul className='list-disc pl-[50px]'>
-                <li>Everything in your industry that happens text with their software passage</li>
-                <li>Distribution patterns may not be as critical the text with their software</li>
+        <div className='md:pl-[200px] max-md:mx-[10px] mt-[10px] mb-[100px] text-left'>
+            <p className='text-[#1e293b] font-semibold text-left text-[25px] max-md:text-[20px] plusjak'>Making Perfect Placeholder</p>
+            <ul className='list-disc pl-[50px] max-md:text-[17px] dmsans'>
+                <li className='mb-[5px]'>Everything in your industry that happens text with their software passage</li>
+                <li className='mb-[5px]'>Distribution patterns may not be as critical the text with their software</li>
                 <li>Analysis is part of good management history of lorem ipsum the text with their</li>
             </ul>
         </div>
@@ -45,4 +45,4 @@ const Blog2 = (props) => {
   )
 }
 
-export default Blog2
+export default Blog1
